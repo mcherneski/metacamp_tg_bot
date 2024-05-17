6d90051e879002e87cec68463c5f47c083203385
Mike Cherneski <mgcherneski@gmail.com>
2024-05-17 10:21:27 -0600

    Added secrets handle in after_install and created config files for .env files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+SECRET_NAME='BOT_TOKEN'
+REGION='us-west-2'
+SECRET_VALUE=$(aws ssm get-parameter --name "$SECRET_NAME" --with-decryption --region "$REGION" --query "Parameter.Value" --output text)
+
+echo "export BOT_TOKEN=$SECRET_VALUE" >> /home/ec2-user/metacamp_tg_bot/.env
+
 echo 'run after_install.sh' >> /home/ec2-user/metacamp_tg_bot/deploy.log
 
 echo 'cd /home/ec2-user/metacamp_tg_bot' >> /home/ec2-user/metacamp_tg_bot/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'run application_start.sh' >> /home/ec2-user/metacamp_tg_bot/deploy.log
 
-echo 'pm2 restart nodejs-app'  >> /home/ec2-user/metacamp_tg_bot/deploy.log
-pm2 restart nodejs-app >> /home/ec2-user/metacamp_tg_bot/deploy.log
+echo 'pm2 startOrRestart using config file'  >> /home/ec2-user/metacamp_tg_bot/deploy.log
+pm2 startOrRestart ../ecosystem.config.js >> /home/ec2-user/metacamp_tg_bot/deploy.log
