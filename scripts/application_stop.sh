a916ed5e96f666aba030ac8cd3038e18cef7b6d1
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 12:26:45 -0600

    Added directory removal to stop script

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,2 +1,4 @@
 echo 'Stopping pm2' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 pm2 stop metacamp_tg_bot >> /home/ec2-user/metacamp_bot_logs/deploy.log
+echo 'Removing old directory' >> /home/ec2-user/metacamp_bot_logs/deploy.log
+rm -rf /home/ec2-user/metacamp_tg_bot/dist >> /home/ec2-user/metacamp_bot_logs/deploy.log
