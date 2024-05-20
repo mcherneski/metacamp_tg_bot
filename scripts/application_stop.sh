c88362c289c4385ab0b73db3a174d04c213bdc9a
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 14:30:34 -0600

    Combined application_stop and before_install commands in appspec.yml

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,3 +2,10 @@ echo 'Stopping pm2' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 pm2 stop metacamp_tg_bot >> /home/ec2-user/metacamp_bot_logs/deploy.log
 echo 'Removing old directory' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 rm -rf /home/ec2-user/metacamp_tg_bot/dist >> /home/ec2-user/metacamp_bot_logs/deploy.log
+
+echo 'Cleaning up old files' >> /home/ec2-user/metacamp_bot_logs/deploy.log
+DEPLOY_DIR=/home/ec2-user/metacamp_tg_bot
+
+if [ -d "$DEPLOY_DIR" ]; then
+    rm -rf "$DEPLOY_DIR/*"
+fi 
