7f974a2432e76017e9c621c8249bd73e46944946
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 20:00:44 -0600

    updated application_start to remove directory structure

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,4 @@
 echo 'run application_start.sh' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 
 echo 'pm2 startOrRestart using config file'  >> /home/ec2-user/metacamp_bot_logs/deploy.log
-pm2 startOrRestart ../ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
+pm2 startOrRestart ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
