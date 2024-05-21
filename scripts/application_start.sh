fade39b3bdbe6c075c21cfd12b49746fd49d2f5c
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 20:17:27 -0600

    fixing some pm2 stuff.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,5 @@
 echo 'run application_start.sh' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 
 echo 'pm2 startOrRestart using config file'  >> /home/ec2-user/metacamp_bot_logs/deploy.log
-pm2 startOrRestart ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
+pm2 stop all
+pm2 start /home/ec2-user/metacamp-tg-bot/ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Cleaning up old files' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 DEPLOY_DIR=/home/ec2-user/metacamp_tg_bot
-
+sudo npm install pm2 -g
 if [ -d "$DEPLOY_DIR" ]; then
     rm -rf "/home/ec2-user/metacamp_tg_bot"
 fi 
