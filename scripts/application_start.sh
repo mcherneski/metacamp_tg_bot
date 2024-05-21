f4c19845efcf353b145a4d0a099786ffbf18db28
Mike Cherneski <mgcherneski@gmail.com>
2024-05-21 09:27:56 -0600

    changed run permissions

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'run application_start.sh' >> /home/ec2-user/metacamp_bot_logs/deploy.log
-sudo npm install pm2 -g >> /home/ec2-user/metacamp_bot_logs/deploy.log
+npm install pm2 -g >> /home/ec2-user/metacamp_bot_logs/deploy.log
 echo 'pm2 startOrRestart using config file'  >> /home/ec2-user/metacamp_bot_logs/deploy.log
-sudo pm2 stop all
-sudo pm2 start /home/ec2-user/metacamp-tg-bot/ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
+pm2 stop all
+pm2 start /home/ec2-user/metacamp-tg-bot/ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
