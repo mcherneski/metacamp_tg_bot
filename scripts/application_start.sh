dbda72c97ddf5e871bfe9599ab8c1c48c19e12d7
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 20:37:27 -0600

    pm2 blues

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'run application_start.sh' >> /home/ec2-user/metacamp_bot_logs/deploy.log
-
+npm install pm2 -g >> /home/ec2-user/metacamp_bot_logs/deploy.log
 echo 'pm2 startOrRestart using config file'  >> /home/ec2-user/metacamp_bot_logs/deploy.log
 pm2 stop all
 pm2 start /home/ec2-user/metacamp-tg-bot/ecosystem.config.js >> /home/ec2-user/metacamp_bot_logs/deploy.log
