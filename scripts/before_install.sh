55a58b2cbf74acb579e542ed0b1e415ce53fdda3
Mike Cherneski <mgcherneski@gmail.com>
2024-05-20 18:35:05 -0600

    direct removal in before_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,5 +3,5 @@ echo 'Cleaning up old files' >> /home/ec2-user/metacamp_bot_logs/deploy.log
 DEPLOY_DIR=/home/ec2-user/metacamp_tg_bot
 
 if [ -d "$DEPLOY_DIR" ]; then
-    rm -rf "$DEPLOY_DIR/*"
+    rm -rf "/home/ec2-user/metacamp_tg_bot"
 fi 
