c3e3334fc0335fd2fff3a69f2f2984c234338d09
Mike Cherneski <mgcherneski@gmail.com>
2024-05-22 12:56:57 -0600

    Trying a new approach to application_start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pm2 stop all
-pm2 start /home/ec2-user/metacamp-tg-bot/ecosystem.config.js
+~/.nvm/versions/node/v20.13.1/bin/pm2 status
+~/.nvm/versions/node/v20.13.1/bin/pm2 startOrRestart /home/ec2-user/metacamp-tg-bot/ecosystem.config.js
