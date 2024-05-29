db63014e0e5e9a17710dacf9872acfb4a4fbd5d7
Mike Cherneski <mgcherneski@gmail.com>
2024-05-28 22:07:54 -0600

    Updated newSession within createSession query

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -137,11 +137,11 @@ export const createSession = async (creator: string, name: string, description: 
       creatortg = creator
    }
    
-   const creatorUser = await prisma.user.findFirst({
-      where: { telegram_id: {equals: creatortg, mode: 'insensitive'}}
-   })
+   // const creatorUser = await prisma.user.findFirst({
+   //    where: { telegram_id: {equals: creatortg, mode: 'insensitive'}}
+   // })
 
-   const creatorId = creatorUser?.telegram_id || ''
+   // const creatorId = creatorUser.telegram_id
    
    const newSession = await prisma.session.create({
       data: {
@@ -150,7 +150,7 @@ export const createSession = async (creator: string, name: string, description: 
          date: date,
          time: time,
          location: location,
-         creatorId: creatorId
+         creatorId: creatortg
       }
    })
    return newSession
